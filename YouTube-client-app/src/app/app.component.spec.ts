0d420716e3c08ce4480d4f099620ad670e4af28c
Natalia Bandurova <natacsp@mail.ru>
2024-07-03 14:37:57 +0300

    feat: add components to app.component

--- YouTube-client-app/src/app/app.component.spec.ts
+++ YouTube-client-app/src/app/app.component.spec.ts
@@ -25,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, YouTube-client-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, YouTube-client-app',
+    );
   });
 });
--- YouTube-client-app/src/app/app.component.ts
+++ YouTube-client-app/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+  ],
 })
 export class AppComponent {
   title = 'YouTube-client-app';
