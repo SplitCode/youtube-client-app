0962e70253adcb33ec25e4d85bc2b9038748cd85
Natalia Bandurova <natacsp@mail.ru>
2024-07-05 14:45:33 +0300

    refactor: change structure

--- YouTube-client-app/src/app/app.component.ts
+++ YouTube-client-app/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 import { FilterComponent } from './components/filter/filter.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SearchItemComponent } from './components/search/search-item/search-item.component';
-import { SearchResultsComponent } from './components/search/search-results/search-results.component';
+import { SearchItemComponent } from './components/search-results/search-item/search-item.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 @Component({
   selector: 'app-root',
