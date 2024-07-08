c52ada66c85c9f4314990af9729e83bc73fb5100
Natalia Bandurova <natacsp@mail.ru>
2024-07-08 13:20:09 +0300

    feat: add styles for search input

--- YouTube-client-app/src/app/components/header/header.component.ts
+++ YouTube-client-app/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { LoginInfoComponent } from './login-info/login-info.component';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { SettingsButtonComponent } from './settings-button/settings-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchInputComponent],
+  imports: [SearchInputComponent, SettingsButtonComponent, LoginInfoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
