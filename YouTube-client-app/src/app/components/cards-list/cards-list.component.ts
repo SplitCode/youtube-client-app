f12a868680823faa71f5f071db42cc9773f347fe
Natalia Bandurova <natacsp@mail.ru>
2024-07-09 17:51:40 +0300

    feat: add cards list

--- YouTube-client-app/src/app/components/cards-list/cards-list.component.ts
+++ YouTube-client-app/src/app/components/cards-list/cards-list.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { CardItemComponent } from './card-item/card-item.component';
@@ -6,22 +7,63 @@ import { CardItemModel } from './models/card-item.model';
 @Component({
   selector: 'app-cards-list',
   standalone: true,
-  imports: [CardItemComponent],
+  imports: [CommonModule, CardItemComponent],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.scss',
 })
 export class CardsListComponent {
-  cardItem: CardItemModel = {
-    kind: 'youtube#video',
-    etag: '"Fznwjl6JEQdo1MGvHOGaz_YanRU/uto79F2R8W05GFpiUAcLdFGs7PQ"',
-    id: 'Fdf5aTYRW0E',
-    url: 'https://i.ytimg.com/vi/4KBVkQ7b6yk/mqdefault.jpg',
-    viewCount: '1266085',
-    likeCount: '18342',
-    dislikeCount: '473',
-    favoriteCount: '0',
-    commentCount: '1255',
-    channelTitle: 'Programming with Mosh',
-    videoTitle: 'Top 50 Angular Interview Questions',
-  };
+  cardsList: CardItemModel[] = [
+    {
+      kind: 'youtube#video',
+      etag: '"Fznwjl6JEQdo1MGvHOGaz_YanRU/uto79F2R8W05GFpiUAcLdFGs7PQ"',
+      id: 'Fdf5aTYRW0E',
+      url: 'https://i.ytimg.com/vi/4KBVkQ7b6yk/mqdefault.jpg',
+      viewCount: '1266085',
+      likeCount: '18342',
+      dislikeCount: '473',
+      favoriteCount: '0',
+      commentCount: '1255',
+      channelTitle: 'Programming with Mosh',
+      videoTitle: 'Top 50 Angular Interview Questions',
+    },
+    {
+      kind: 'youtube#video',
+      etag: '"Fznwjl6JEQdo1MGvHOGaz_YanRU/uto79F2R8W05GFpiUAcLdFGs7PQ"',
+      id: 'Fdf5aTYRW0E',
+      url: 'https://i.ytimg.com/vi/4KBVkQ7b6yk/mqdefault.jpg',
+      viewCount: '1266085',
+      likeCount: '18342',
+      dislikeCount: '473',
+      favoriteCount: '0',
+      commentCount: '1255',
+      channelTitle: 'Programming with Mosh',
+      videoTitle: 'Top 50 Angular Interview Questions',
+    },
+    {
+      kind: 'youtube#video',
+      etag: '"Fznwjl6JEQdo1MGvHOGaz_YanRU/uto79F2R8W05GFpiUAcLdFGs7PQ"',
+      id: 'Fdf5aTYRW0E',
+      url: 'https://i.ytimg.com/vi/4KBVkQ7b6yk/mqdefault.jpg',
+      viewCount: '1266085',
+      likeCount: '18342',
+      dislikeCount: '473',
+      favoriteCount: '0',
+      commentCount: '1255',
+      channelTitle: 'Programming with Mosh',
+      videoTitle: 'Top 50 Angular Interview Questions',
+    },
+    {
+      kind: 'youtube#video',
+      etag: '"Fznwjl6JEQdo1MGvHOGaz_YanRU/uto79F2R8W05GFpiUAcLdFGs7PQ"',
+      id: 'Fdf5aTYRW0E',
+      url: 'https://i.ytimg.com/vi/4KBVkQ7b6yk/mqdefault.jpg',
+      viewCount: '1266085',
+      likeCount: '18342',
+      dislikeCount: '473',
+      favoriteCount: '0',
+      commentCount: '1255',
+      channelTitle: 'Programming with Mosh',
+      videoTitle: 'Top 50 Angular Interview Questions',
+    },
+  ];
 }
