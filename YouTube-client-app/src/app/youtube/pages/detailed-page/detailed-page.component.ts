0067138a81fb714c8718f8bb0e0876f7985a31c2
Natalia Bandurova <natacsp@mail.ru>
2024-07-15 17:27:33 +0300

    feat: create detailed-page module

---
+++ YouTube-client-app/src/app/youtube/pages/detailed-page/detailed-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './detailed-page.component.html',
+  styleUrl: './detailed-page.component.scss'
+})
+export class DetailedPageComponent {
+
+}
