4d04a2db48947358e156fa30ef21453ee701eaa3
Natalia Bandurova <natacsp@mail.ru>
2024-07-17 13:18:43 +0300

    feat: delete app module

--- YouTube-client-app/src/app/app.component.ts
+++ YouTube-client-app/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HeaderComponent } from './core/components/header/header.component';
+// import { FooterComponent } from './core/components/footer/footer.component';
+// import { HeaderComponent } from './core/components/header/header.component';
 // import { MainPageComponent } from './youtube/pages/main-page/main-page.component';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, CoreModule],
 })
 export class AppComponent {
   title = 'YouTube-client-app';
