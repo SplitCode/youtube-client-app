31dd28f9bbef7f06dffff85afe7159cd6e5539c7
Natalia Bandurova <natacsp@mail.ru>
2024-07-17 19:03:58 +0300

    feat: add logout button

--- YouTube-client-app/src/app/core/components/header/header.component.ts
+++ YouTube-client-app/src/app/core/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AuthService } from '../../../auth/services/auth.service';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { FilterToggleService } from '../../../youtube/services/filter-toggle.service';
 import { LoginInfoComponent } from './login-info/login-info.component';
 import { SearchInputComponent } from './search-input/search-input.component';
@@ -16,14 +19,22 @@ import { SettingsButtonComponent } from './settings-button/settings-button.compo
     LoginInfoComponent,
     CommonModule,
     SettingsButtonComponent,
+    ButtonComponent,
   ],
 })
 export class HeaderComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
   public isFilterShown: boolean = false;
 
   constructor(private filterToggleService: FilterToggleService) {}
 
   toggleFilter(): void {
     this.filterToggleService.toggleFilterShow();
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
