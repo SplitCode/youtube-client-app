077b3cfa23e19e752864f521352b85da6d3f8c03
Natalia Bandurova <natacsp@mail.ru>
2024-07-17 16:35:31 +0300

    feat: add login form

--- YouTube-client-app/src/app/auth/pages/login/login.component.ts
+++ YouTube-client-app/src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  login: string = '';
+  password: string = '';
 }
