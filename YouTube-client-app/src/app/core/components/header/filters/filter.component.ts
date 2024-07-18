4f43c300a252becc9f0f5d1fe81e451972750c9d
Natalia Bandurova <natacsp@mail.ru>
2024-07-18 23:44:38 +0300

    feat: add debounce to sort input

--- YouTube-client-app/src/app/core/components/header/filters/filter.component.ts
+++ YouTube-client-app/src/app/core/components/header/filters/filter.component.ts
@@ -1,13 +1,15 @@
 import {
   Component, EventEmitter, inject, Output
 } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 import { SearchService } from '../../../services/search.service';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss',
 })
@@ -15,18 +17,26 @@ export class FilterComponent {
   searchService = inject(SearchService);
   @Output() wordFilterChange: EventEmitter<string> = new EventEmitter();
 
+  filterControl = new FormControl('');
+
+  constructor() {
+    this.filterControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe((value: string | null) => {
+      if (value !== null) {
+        this.searchService.updateFilterWord(value);
+        this.wordFilterChange.emit(value);
+      }
+    });
+  }
+
   onDateClick() {
     this.searchService.updateDateSortClick();
   }
 
   onViewsClick() {
     this.searchService.updateViewSortClick();
-  }
-
-  onFilterWordChange(event: Event) {
-    const filterWord = (event.target as HTMLInputElement).value;
-    this.searchService.updateFilterWord(filterWord);
-    this.wordFilterChange.emit(filterWord);
   }
 
   preventSubmit(event: Event) {
