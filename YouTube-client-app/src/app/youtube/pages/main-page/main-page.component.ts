c0d3a700ca39575a991f8023249e4b8696a686f2
Natalia Bandurova <natacsp@mail.ru>
2024-07-22 10:35:02 +0300

    fix: use async pipe instead of subscribe

--- YouTube-client-app/src/app/youtube/pages/main-page/main-page.component.ts
+++ YouTube-client-app/src/app/youtube/pages/main-page/main-page.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import {
-  Component, inject, OnDestroy, OnInit
+  Component, DestroyRef, inject, OnInit
 } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+// import { Subject, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 import { SearchService } from '../../../core/services/search.service';
 import { CardShowService } from '../../services/card-show.service';
@@ -15,30 +17,26 @@ import { CardsListComponent } from './cards-list/cards-list.component';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent implements OnInit, OnDestroy {
+export class MainPageComponent implements OnInit {
   private cardShowService = inject(CardShowService);
   private searchService = inject(SearchService);
-  private destroy$ = new Subject<void>();
+  private destroyRef = inject(DestroyRef);
 
-  public isCardsShown: boolean = false;
-  public filterWord: string = '';
+  public isCardsShown$: Observable<boolean>;
+  public filterWord$: Observable<string>;
 
-  ngOnInit(): void {
-    this.cardShowService.submitForm$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((value: boolean) => {
-        this.isCardsShown = value;
-      });
-
-    this.searchService.currentFilterWord$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((word: string) => {
-        this.filterWord = word;
-      });
+  constructor() {
+    this.isCardsShown$ = this.cardShowService.submitForm$;
+    this.filterWord$ = this.searchService.currentFilterWord$;
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
+  ngOnInit(): void {
+    this.isCardsShown$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe();
+
+    this.filterWord$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe();
   }
 }
