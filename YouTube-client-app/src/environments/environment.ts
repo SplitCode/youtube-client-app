1214dd9becceabe9382db8ca62fce09730eb540a
Natalia Bandurova <natacsp@mail.ru>
2024-07-22 13:34:35 +0300

    feat: add environments

---
+++ YouTube-client-app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://www.googleapis.com/youtube/v3',
+  API_KEY: 'AIzaSyDGmnFGn-vaGF65YM6rmL7HEvrnEPLnrxI',
+};
+
+// const API_KEY = 'AIzaSyDGmnFGn-vaGF65YM6rmL7HEvrnEPLnrxI';
+// export default API_KEY;
