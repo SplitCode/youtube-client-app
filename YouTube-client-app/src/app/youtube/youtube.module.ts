c4df4d32a40d810906d5a59986b20643f026c335
Natalia Bandurova <natacsp@mail.ru>
2024-07-25 12:32:26 +0300

    fix: delete commented code

--- YouTube-client-app/src/app/youtube/youtube.module.ts
+++ YouTube-client-app/src/app/youtube/youtube.module.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-// скорее всего тоже нет смвсла в импортре, так как модуль загружается через роутинг
-// import { DetailedPageComponent } from './pages/detailed-page/detailed-page.component';
 import { YoutubeRoutingModule } from './youtube-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    // DetailedPageComponent,
     YoutubeRoutingModule,
   ],
 })
