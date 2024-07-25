1eca03bba81aa60e6ee5ae8dc5ff7d8e38193aa7
Natalia Bandurova <natacsp@mail.ru>
2024-07-25 10:38:11 +0300

    fix: fix env

--- YouTube-client-app/src/environments/environment.ts
+++ YouTube-client-app/src/environments/environment.ts
@@ -3,6 +3,3 @@ export const environment = {
   API_URL: 'https://www.googleapis.com/youtube/v3',
   API_KEY: 'AIzaSyDGmnFGn-vaGF65YM6rmL7HEvrnEPLnrxI',
 };
-
-// const API_KEY = 'AIzaSyDGmnFGn-vaGF65YM6rmL7HEvrnEPLnrxI';
-// export default API_KEY;
