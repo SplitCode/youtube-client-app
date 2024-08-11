78f2492c80a306f3f97c65728f8ca8f24b0e5aa6
Natalia Bandurova <natacsp@mail.ru>
2024-08-12 00:04:25 +0300

    chore: create jest.config

---
+++ YouTube-client-app/jest.config.js
@@ -0,0 +1,33 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+const customModulePaths = pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' });
+
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
+      diagnostics: false
+    }
+  },
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  testEnvironment: 'jest-environment-jsdom-thirteen',
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+    ...customModulePaths
+  },
+  transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js'],
+  coverageReporters: ['html'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts']
+};
