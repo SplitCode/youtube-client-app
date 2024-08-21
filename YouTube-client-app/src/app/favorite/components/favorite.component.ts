60d911554f304ed936c2e99c5c9177b666bde01c
Natalia Bandurova <natacsp@mail.ru>
2024-08-21 13:45:33 +0300

    refactor: simplify favorite comp init

--- YouTube-client-app/src/app/favorite/components/favorite.component.ts
+++ YouTube-client-app/src/app/favorite/components/favorite.component.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
 
 import { selectFavoriteCards } from '../../redux/selectors/card.selector';
-import { CardItemModel } from '../../youtube/models/card-item.model';
 import { CardItemComponent } from '../../youtube/pages/cards-list-page/card-item/card-item.component';
 
 @Component({
@@ -15,10 +13,6 @@ import { CardItemComponent } from '../../youtube/pages/cards-list-page/card-item
   styleUrl: './favorite.component.scss',
 })
 export class FavoriteComponent {
-  favoriteVideos$: Observable<CardItemModel[]>;
   store = inject(Store);
-
-  constructor() {
-    this.favoriteVideos$ = this.store.select(selectFavoriteCards);
-  }
+  favoriteVideos$ = this.store.select(selectFavoriteCards);
 }
